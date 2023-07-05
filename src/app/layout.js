a8161dcf87b782472da6edab99d5a9cf44260a9d
Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
2023-07-05 07:52:19 +0000

    dark mode functionality implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import NextThemeProvider from "@/themes/provider";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -14,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={`${poppins.className} bg-[#FBFCFE] dark:bg-[#161C27]`}>
+        <NextThemeProvider>{children}</NextThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import {
   Home,
   User,
@@ -10,49 +10,70 @@ import {
   Sun,
   Moon,
 } from "react-feather";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  const [themeState, setThemeState] = useState("light");
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  const toggleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
-    <header className="max-w-screen-lg mx-auto">
-      <nav>
-        <a href="/">Sanchit Bajaj</a>
+    <header className={`py-4`}>
+      <nav className="max-w-screen-lg mx-auto flex items-center">
+        <a href="/" className="text-xl">
+          Sanchit Bajaj
+        </a>
 
-        <div>
-          <ul>
-            <li>
-              <a href="/">
+        <div className="ml-auto">
+          <ul className="flex items-center gap-4">
+            <li className="">
+              <a href="/" className="flex gap-1 items-center">
                 <Home size={20} /> Home
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" className="flex gap-1 items-center">
                 <User size={20} /> About
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" className="flex gap-1 items-center">
                 <FileText size={20} /> Skills
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" className="flex gap-1 items-center">
                 <Briefcase size={20} /> Services
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" className="flex gap-1 items-center">
                 <Layout size={20} /> Porfolio
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="/" className="flex gap-1 items-center">
                 <Send size={20} /> Contact
               </a>
             </li>
-            <li>
-              {themeState === "light" ? <Sun size={20}/> : <Moon size={20} />}
+            <li onClick={toggleTheme} className="hover:cursor-pointer">
+              {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
             </li>
           </ul>
         </div>
