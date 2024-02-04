53425436918d6667f77961c8e29ef508c9908f3d
Sanchit Bajaj <sanchit02@outlook.com>
2024-02-05 00:10:49 +0530

    update: project upgrade to typescript and nextjs 14

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,3 +17,4 @@ module.exports = {
   },
   plugins: [],
 };
+export default config;
