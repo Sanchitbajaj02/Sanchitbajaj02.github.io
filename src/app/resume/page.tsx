e65d381f01a95fd2de21fbb912e42c050f1be18d
Sanchit Bajaj <sanchit02@outlook.com>
2025-01-22 17:20:31 +0530

    config for error fixes

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -35,22 +35,22 @@ const skills = [
 
 type SkillItem = (typeof skills)[0];
 
-const SkillBadge = ({ skill }: { skill: SkillItem }) => {
-  return (
-    <li className="skills-item">
-      <div className="title-wrapper">
-        <h5 className="h5">{skill.skillName}</h5>
-        <data value={skill.percentage}>{skill.percentage}</data>
-      </div>
-      <div className="skill-progress-bg">
-        <div
-          className="skill-progress-fill"
-          style={{ width: skill.percentage }}
-        ></div>
-      </div>
-    </li>
-  );
-};
+// const SkillBadge = ({ skill }: { skill: SkillItem }) => {
+//   return (
+//     <li className="skills-item">
+//       <div className="title-wrapper">
+//         <h5 className="h5">{skill.skillName}</h5>
+//         <data value={skill.percentage}>{skill.percentage}</data>
+//       </div>
+//       <div className="skill-progress-bg">
+//         <div
+//           className="skill-progress-fill"
+//           style={{ width: skill.percentage }}
+//         ></div>
+//       </div>
+//     </li>
+//   );
+// };
 
 const SkillChip = ({ skill }: { skill: SkillItem }) => {
   return (
